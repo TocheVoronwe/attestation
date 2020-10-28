52b6a4d004a9c3725b537200c900fff542f1122c
Toche <christophe@flitdesk.com>
2020-10-28 21:36:47 +0100

    remove 'no-console rule'

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
 	},
 	extends: ["standard"],
 	rules: {
-		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		/*"no-console": process.env.NODE_ENV === "production" ? "error" : "off",*/
 		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
 		"comma-dangle": [2, "always-multiline"],
 		"no-var": 2,
